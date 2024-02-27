3ed05a8ad5984be9f729ec94770667d2cdfaad61
Yousef <yousefsameh24@gmail.com>
2024-02-27 16:18:58 +0200

    Add Hello component with user list and hello message

---
+++ src/client/src/components/Hello.tsx
@@ -0,0 +1,17 @@
+import { trpc } from '../utils/trpc'
+
+export default function Hello() {
+  const { data, isLoading } = trpc.user.list.useSWR()
+  const { data: hello } = trpc.hello.useSWR('world')
+  if (isLoading) return <div>Loading...</div>
+    return (
+      <div className="container p-12 flex flex-col gap-2">
+        <h1 className="text-4xl font-bold">{hello}</h1>
+        <ul>
+            {data?.map((user) => (
+            <li key={user.id}>{user.name}</li>
+            ))}
+        </ul>
+      </div>
+    )
+}
