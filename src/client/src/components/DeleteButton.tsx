3e3996c95ce7491e228020413acdaf8771d4cdc0
Yousef <yousefsameh24@gmail.com>
2024-02-27 18:51:45 +0200

    Update DeleteButton styling

--- src/client/src/components/DeleteButton.tsx
+++ src/client/src/components/DeleteButton.tsx
@@ -9,7 +9,7 @@ export default function DeleteButton({id, mutate}:{id: number, mutate: () => voi
   }
 
   return (
-    <button onClick={handleDelete} disabled={deleteIsMutating} className="rounded-md transition-colors bg-red-200/20 hover:bg-red-200/40 border px-4 py-2">
+    <button onClick={handleDelete} disabled={deleteIsMutating} className="rounded-md transition-colors bg-red-200/20 hover:bg-red-200/40 border border-red-200/60 px-4 py-2">
         {deleteIsMutating ? <span>Deleting...</span> : <span>Delete</span>}
     </button>
   )
