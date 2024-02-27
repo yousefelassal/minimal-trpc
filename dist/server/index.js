852bd4dc325d9e51d41987deb7a12c43c0ec3a10
Yousef <yousefsameh24@gmail.com>
2024-02-27 22:51:42 +0200

    build server

---
+++ dist/server/index.js
@@ -0,0 +1,33 @@
+import * as trpcExpress from '@trpc/server/adapters/express';
+import express from 'express';
+import cors from 'cors';
+import { userRouter } from './routes/user.js';
+import { publicProcedure, router } from './trpc.js';
+import { z } from 'zod';
+const appRouter = router({
+    user: userRouter,
+    hello: publicProcedure
+        .input(z.string().nullish())
+        .query(({ input }) => `Hello, ${input}!`)
+});
+async function main() {
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+    app.use('/trpc', trpcExpress.createExpressMiddleware({
+        router: appRouter
+    }));
+    app.get('/', (_req, res) => {
+        res.send('hello from api!');
+    });
+    app.get('/version', (_req, res) => {
+        res.send('0.0.1');
+    });
+    app.listen(3000, () => {
+        console.log('Listening on http://localhost:3000');
+    });
+}
+main().catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
---
+++ dist/server/routes/user.js
@@ -0,0 +1,18 @@
+import { publicProcedure, router } from '../trpc.js';
+import { z } from 'zod';
+import { db } from '../db.js';
+export const userRouter = router({
+    list: publicProcedure
+        .query(() => db.user.findMany({
+        orderBy: { createdAt: 'desc' }
+    })),
+    create: publicProcedure
+        .input(z.object({ name: z.string() }))
+        .mutation(({ input }) => db.user.create({ data: input })),
+    delete: publicProcedure
+        .input(z.object({ id: z.number() }))
+        .mutation(({ input }) => db.user.delete({ where: { id: input.id } })),
+    update: publicProcedure
+        .input(z.object({ id: z.number(), name: z.string() }))
+        .mutation(({ input }) => db.user.update({ where: { id: input.id }, data: { name: input.name } })),
+});
