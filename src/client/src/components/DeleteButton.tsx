218569e6792e066f434f2933fd17fbd4161a2906
Yousef <yousefsameh24@gmail.com>
2024-02-27 18:48:37 +0200

    Add DeleteButton component

---
+++ src/client/src/components/DeleteButton.tsx
@@ -0,0 +1,16 @@
+import { trpc } from '../utils/trpc'
+
+export default function DeleteButton({id, mutate}:{id: number, mutate: () => void}) {
+  const { trigger: deleteUser, isMutating: deleteIsMutating } = trpc.user.delete.useSWRMutation()
+
+  const handleDelete = async () => {
+    await deleteUser({ id })
+    mutate()
+  }
+
+  return (
+    <button onClick={handleDelete} disabled={deleteIsMutating} className="rounded-md transition-colors bg-red-200/20 hover:bg-red-200/40 border px-4 py-2">
+        {deleteIsMutating ? <span>Deleting...</span> : <span>Delete</span>}
+    </button>
+  )
+}
